d4d77477752dbfe30459896ac4b4d1896a66aa3a
自宅警备员 <61686557+lsbst1986@users.noreply.github.com>
2023-02-07 11:38:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,18 @@ sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
+
+cat>delete.sh<<-\EOF
+#!/bin/bash
+rm -rf  bin/targets/x86/64/config.buildinfo
+rm -rf  bin/targets/x86/64/feeds.buildinfo
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.vmdk
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.vmdk
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-rootfs.img.gz
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic.manifest
+rm -rf  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz
+rm -rf bin/targets/x86/64/sha256sums
+rm -rf  bin/targets/x86/64/version.buildinfo
+exit 0
+EOF
